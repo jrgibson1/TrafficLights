1d2bd7943090a5794f282cf94aee0a3eab3163bc
John Gibson <jr@jrgibson.com.au>
2017-12-13 19:16:17 +1100

    Initial Commit

---
+++ trafficLightsv2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  trafficLightsv2
+//
+//  Created by John Gibson on 13/12/17.
+//  Copyright © 2017 John Gibson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
